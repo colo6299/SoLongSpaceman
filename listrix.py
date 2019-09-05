167a9d4c4df1368a5984fcc4739704fcacbd5e7f
= <=>
2019-09-05 15:15:59 -0700

    Added functionality to Listrix

--- listrix.py
+++ listrix.py
@@ -9,15 +9,34 @@ class Listrix3:
         self.has_inflated = False
 
     len_list = list()
+    
+    # Histogram list matrix => histrix => hstrx
+    hstrx_len = list()
+    hstrx_wid = list()
+    hstrx_dep = list()
 
-    def inflate(self, length: int, width: int, depth: int = 26):
-        len_list = []
+    def explode(self, length: int, width: int, depth: int = 26):
+        self.len_list = []
         for i in range(length):
-            len_list.append(list())
+            self.len_list.append(list())
             for j in range(width):
-                len_list[i].append([False] * depth)
+                self.len_list[i].append([False] * depth)
+
+    def axial_len(self, width: int, depth: int):
+        retrix = list()
+        for listrix in self.len_list:
+                retrix.append(listrix[width][depth])
+        return retrix
+    
+    def axial_wid(self, length: int, depth: int):
+        retrix = list()
+        for lisrix in self.len_list[length]:
+            retrix.append(lisrix[depth])
+        return retrix
+    
+    def axial_dep(self, length: int, width: int):
+        return self.len_list[length][width]
+
 
 l = Listrix3()
-l.inflate(4,4,26)
-
-print(l.len_list)
+l.explode(100,4,26)
